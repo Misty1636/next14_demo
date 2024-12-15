272920b13b20d60aa8b388cb525e50b3a528ba92
Misty <misty@MistydeMacBook-Pro.local>
2024-12-15 16:59:16 +0800

    test3

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,6 @@ const nextConfig: NextConfig = {
     /* config options here */
     distDir: 'docs',
     output: 'export',
-    assetPrefix: '/next14_demo/', // 設定靜態資源的基礎路徑
     basePath: '/next14_demo',
 };
 
